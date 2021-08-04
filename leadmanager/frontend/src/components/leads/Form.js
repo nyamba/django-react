1c6b5d77707e1f9d2452a45ccceb773c0480e556
Nyambayar Turbat <t.nyambayar@gmail.com>
2021-08-04 10:34:05 +0800

    Dashboard

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+	render(){
+		return (<h2>Form</h2>)
+	}
+}
+
+export default Form
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Leads extends Component {
+	render(){
+		return (<h2>Leads List</h2>)
+	}
+}
+
+export default Leads
